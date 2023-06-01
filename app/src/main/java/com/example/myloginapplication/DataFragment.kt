47783ed5706eb580cba795d989031e24fb80f15e
tommaso <tommymassa98@gmail.com>
2023-06-01 15:01:33 +0200

    Chiamata rest

--- app/src/main/java/com/example/myloginapplication/DataFragment.kt
+++ app/src/main/java/com/example/myloginapplication/DataFragment.kt
@@ -1,12 +1,23 @@
 package com.example.myloginapplication
 
+import android.os.Build
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.os.StrictMode
+import android.os.StrictMode.ThreadPolicy
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
-import androidx.navigation.fragment.findNavController
+import androidx.fragment.app.Fragment
+import okhttp3.OkHttpClient;
+
+import okhttp3.Request
+import java.io.IOException
+import java.io.InputStream
+import java.net.SocketTimeoutException
+import java.nio.charset.Charset
+import java.util.concurrent.TimeUnit
+
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -32,9 +43,45 @@ class DataFragment : Fragment() {
         val view = inflater.inflate(R.layout.fragment_data, container, false)
         val button = view.findViewById<Button>(R.id.button2)
 
-        button.setOnClickListener{
+        button.setOnClickListener {
             button.setText("CIAO")
+
+
+            val SDK_INT = Build.VERSION.SDK_INT
+            if (SDK_INT > 8) {
+                val policy = ThreadPolicy.Builder()
+                    .permitAll().build()
+                StrictMode.setThreadPolicy(policy)
+                try {
+                    val client = OkHttpClient.Builder()
+                        .callTimeout(30000, TimeUnit.MILLISECONDS)
+                        .build()
+
+                    val request = Request.Builder()
+                        .url("https://apidojo-yahoo-finance-v1.p.rapidapi.com/auto-complete?q=tesla&region=US")
+                        .get()
+                        .addHeader(
+                            "X-RapidAPI-Key",
+                            "0f3731783dmshbf77e223a93fd29p17b87fjsn662b2e150b17"
+                        )
+                        .addHeader("X-RapidAPI-Host", "apidojo-yahoo-finance-v1.p.rapidapi.com")
+                        .build()
+
+
+                    val response = client.newCall(request).execute()
+                    println(response.body.toString())
+
+                } catch (e: SocketTimeoutException){
+                    println(e.toString())
+                }
+
+
+
+            }
         }
+
+
+
         return view
     }
 
